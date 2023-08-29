40f0bc6d1f8f2a258923b71172342ef3e9b1ce6d
Alexander Kostylev <successful08@mail.ru>
2023-08-30 00:22:29 +0500

    Example 005 If_Else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя ");
+string userName=Console.ReadLine();
+
+if(userName.ToLower() == "alex")
+{
+    Console.WriteLine("Ура, это же Alex");
+}
+
+else 
+{
+    Console.Write("Привет ");
+    Console.Write(userName);
+}
